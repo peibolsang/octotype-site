f5582e7e6c59f84cd2cdae8e673c2266868291ba
Pablo Bermejo <peibolsang@gmail.com>
2023-02-21 12:10:59 +0100

    nico change

--- components/main-intro.tsx
+++ components/main-intro.tsx
@@ -9,7 +9,7 @@ const MainIntro = () => {
     <MainHeader />
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-6 mb-12 md:mb-6 align-middle">
       <div className="inline-block h-40 md:h-96 xl:h-auto text-4xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 text-center">
-        The content discovery platform from developers to <Cursor textarray={textarray} />
+        The content discovery platform from Nico to <Cursor textarray={textarray} />
       </div>
     </section>
   </>
