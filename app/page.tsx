66e5926cb6afcc789e566e154430e23aceda8442
pbermejo <pbermejo@guidewire.com>
2024-01-04 14:27:26 +0100

    personalized HowItWorks with username

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
         <Suspense fallback={<AllStoriesSkeleton/>} >
           <AllStoriesServer />
         </Suspense>
-        <HowItWorks />
+        <HowItWorks username={'[user]'} />
     </div>
   );
 }
