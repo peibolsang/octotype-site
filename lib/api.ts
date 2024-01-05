dcd3e2cce5e6df51bf094c17975db166eb02f829
pbermejo <pbermejo@guidewire.com>
2024-01-05 10:21:14 +0100

    ppr with fetch revalidate 0

--- lib/api.ts
+++ lib/api.ts
@@ -26,9 +26,9 @@ export async function fetchGitHubAPI(url: string) {
   const params = {
     method: 'GET',
     headers: headers,
-    //next:{
-    //  revalidate: 300 // fresh data every 5 mins. We don't need to server fresh data in every call. Users can wait 5 mins to see their posts in the platform
-    //}
+    next:{
+      revalidate: 0 // 0 for PPR; 300 for non-PPR.
+    }
   };
 
   const response = await fetch(url, params);
