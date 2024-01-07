314b920189db534039dbcb173a0954c64cfc72f3
pbermejo <pbermejo@guidewire.com>
2024-01-07 18:27:58 +0100

    added new Github markdown rendering and Systax HL

--- components/client/post-body.tsx
+++ components/client/post-body.tsx
@@ -1,16 +1,46 @@
 import markdownStyles from "@/components/ui/markdown-styles.module.css";
+import React from 'react'
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import {coldarkCold} from 'react-syntax-highlighter/dist/esm/styles/prism'
+
 
 type Props = {
   content: string;
 };
 
+
 const PostBody = ({ content }: Props) => {
   return (
     <div className="py-[32px]">
-      <div
-        className={markdownStyles["markdown"]}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+      <div/>
+        <Markdown
+          className={markdownStyles["markdown"]} 
+          remarkPlugins={[remarkGfm]}
+          children={content}
+          components={{
+            code(props){
+             const {children, className, node, ...rest} = props
+             const match = /language-(\w+)/.exec(className || '')
+             return match ? (
+              <SyntaxHighlighter
+                PreTag="div"
+                children={String(children).replace(/\n$/, '')}
+                language={match[1]}
+                style={coldarkCold}
+              />
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            )
+            }
+          }
+
+          }
+        />
+
     </div>
   );
 };
