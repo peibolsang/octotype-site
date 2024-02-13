da7d0c6bbf16886b3e57020c827878b194a54469
pbermejo <pbermejo@guidewire.com>
2024-02-13 09:46:12 +0100

    PPR also in Story page

--- components/server/story.tsx
+++ components/server/story.tsx
@@ -1,7 +1,7 @@
 import { getPost, getPostComments} from "@/lib/api";
 import { StoryClient } from "@/components/client/story";
 import { Story404 } from "@/components/client/errors/story-404";
-import { unstable_cache } from "next/cache";
+import { unstable_noStore as noStore } from "next/cache";
 
 interface StoryServerProps {
   user: string;
@@ -9,25 +9,19 @@ interface StoryServerProps {
 }
  
 
-const getStory = unstable_cache(
-  async(user, slug) =>{
+const getStory = async(user:string, slug:string) =>{
 
     // Parallel fetching
     const postReponse =  getPost(user, slug);
     const commentsReponse = getPostComments(user, slug);
 
     return await Promise.all([postReponse,commentsReponse])
-
-  },
-  ['user-story'],
-  {
-    revalidate: 3600
-  }
-)
+}
 
 // Note: Server components should not use React.FC as they cannot have children or use React's context
 const StoryServer = async ({ user, slug }: StoryServerProps) => {
   
+    noStore()
     const [post, comments] = await getStory(user,slug)
     
     if (!post || !post.content)  {
