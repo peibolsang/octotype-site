eec39122e6bcc8d3a8d598149476f841d5206fcd
pbermejo <pbermejo@guidewire.com>
2024-03-02 23:32:55 +0100

    a

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -3,15 +3,6 @@ import { ImageResponse } from 'next/og'
 // Route segment config
 export const runtime = 'edge'
  
-// Image metadata
-export const alt = 'About Acme'
-export const size = {
-  width: 1200,
-  height: 630,
-}
- 
-export const contentType = 'image/png'
- 
 // Image generation
 export async function GET() {
   // Font
@@ -33,11 +24,5 @@ export async function GET() {
         About Acme
       </div>
     ),
-    // ImageResponse options
-    {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
-      ...size,
-    }
   )
 }
